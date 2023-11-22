2aed993e1bf501c55687e8fd5ba0e8a08a4415be
Suman Singh Bhati <suman.bhati@itgurussoftware.com>
2023-11-22 16:51:11 +0530

    Initial Commit

---
+++ CompanyTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CompanyTask
+//
+//  Created by Apple on 22/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
